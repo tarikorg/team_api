6fdb943cfc86525aa4c66ca281e10bf5e5a25c3f
tarikorg <myku1771@gmail.com>
2024-04-26 14:16:23 -0400

    changed1

--- database.js
+++ database.js
@@ -4,10 +4,7 @@ const is_prod = process.env.NODE_ENV
 
 console.log(is_prod)
 
-const client = is_prod ? new Sequelize(process.env.DATABASE_URL,   {
-    dialect: 'postgres',
-    logging: false
-}) : new Sequelize(
+const client = is_prod ? new Sequelize(process.env.DATABASE_URL) : new Sequelize(
     process.env.DB_NAME,
     process.env.DB_USERNAME,
     process.env.DB_PASSWORD,
